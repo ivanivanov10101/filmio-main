acad77802b39193e8bd4e54f330482fcc2045624
Ivan Ivanov <78234071+ivanivanov10101@users.noreply.github.com>
2024-04-19 18:01:05 +0300

    multiple updates

--- Frontend/src/components/ThemeProvider.tsx
+++ Frontend/src/components/ThemeProvider.tsx
@@ -1,17 +1,19 @@
-import { useAppSelector } from '../store/storeHooks';
+import { useAppSelector } from "../store/storeHooks";
 
 type LayoutProps = {
-    children: React.ReactNode;
+  children: React.ReactNode;
 };
 
 const ThemeProvider = ({ children }: LayoutProps) => {
-    const { theme } = useAppSelector((state) => state.theme);
+  const { theme } = useAppSelector((state) => state.theme);
 
-    return (
-        <div className={theme}>
-            <div className='text-gray-800 bg-white dark:text-gray-200 dark:bg-[rgb(16,23,42)]'>{children}</div>
-        </div>
-    );
+  return (
+    <div className={theme}>
+      <div className="text-gray-800 bg-white dark:text-neutral-50 dark:bg-[#192023]">
+        {children}
+      </div>
+    </div>
+  );
 };
 
 export default ThemeProvider;
