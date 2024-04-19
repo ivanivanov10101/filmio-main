fe4a3a72d4f687ad110106f8151716e7694c4e65
Ivan Ivanov <78234071+ivanivanov10101@users.noreply.github.com>
2024-04-19 17:38:34 +0300

    updates to pages

--- Frontend/src/pages/Dashboard.tsx
+++ Frontend/src/pages/Dashboard.tsx
@@ -1,11 +1,11 @@
-import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
-import DashProfile from '../components/DashBoard/DashProfile';
-import DashPosts from '../components/DashBoard/DashPosts';
-import DashSidebar from '../components/DashBoard/DashSidebar';
-import DashUsers from '../components/DashBoard/DashUsers';
-import DashComments from '../components/DashBoard/DashComments';
-import DashBoardComp from '../components/DashBoard/DashBoardComp';
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import DashProfile from "../components/DashBoard/DashProfile";
+import DashPosts from "../components/DashBoard/DashPosts";
+import DashSidebar from "../components/DashBoard/DashSidebar";
+import DashUsers from "../components/DashBoard/DashUsers";
+import DashComments from "../components/DashBoard/DashComments";
+import DashBoardComp from "../components/DashBoard/DashBoardComp";
 
 const Dashboard = () => {
     const location = useLocation();
@@ -13,20 +13,20 @@ const Dashboard = () => {
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
-        const tabFromUrl = urlParams.get('tab');
+        const tabFromUrl = urlParams.get("tab");
         setTab(tabFromUrl);
     }, [location.search]);
 
     return (
-        <div className='flex flex-col md:flex-row'>
-            <div className='md:min-h-screen min-w-56'>
+        <div className="flex flex-col md:flex-row">
+            <div className="md:min-h-screen min-w-56">
                 <DashSidebar />
             </div>
             {tab === null && <DashBoardComp />}
-            {tab === 'profile' && <DashProfile />}
-            {tab === 'posts' && <DashPosts />}
-            {tab === 'users' && <DashUsers />}
-            {tab === 'comments' && <DashComments />}
+            {tab === "profile" && <DashProfile />}
+            {tab === "posts" && <DashPosts />}
+            {tab === "users" && <DashUsers />}
+            {tab === "comments" && <DashComments />}
         </div>
     );
 };
