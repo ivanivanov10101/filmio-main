efed70c8501d1e0cddabf2506ceb371e410e3eb9
Ivan Ivanov <78234071+ivanivanov10101@users.noreply.github.com>
2024-04-19 17:56:12 +0300

    header footer updates

--- Frontend/src/components/Footer/Footer.tsx
+++ Frontend/src/components/Footer/Footer.tsx
@@ -1,75 +1,72 @@
-import { Footer } from 'flowbite-react';
-import { BsFacebook, BsGithub, BsInstagram, BsLinkedin, BsTwitter } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import { Footer } from "flowbite-react";
+import {
+    BsGithub,
+    BsLinkedin,
+} from "react-icons/bs";
+import { Link } from "react-router-dom";
 
-const FooterCom = () => {
+const FooterComponent = () => {
     return (
-        <Footer container className='border border-t-4 border-teal-500'>
-            <div className='w-full mx-auto max-w-7xl'>
-                <div className='flex flex-col justify-between md:flex-row md:px-14'>
-                    <div className='mb-5 md:mb-0'>
+        <Footer container className="border-t-4 max-h-72 dark:bg-[rgba(49,57,66,1)]">
+            <div className="w-full mx-auto max-w-6xl">
+                <div className="flex flex-col justify-between md:flex-row md:px-14">
+                    <div className="mb-5 md:mb-0">
                         <Link
-                            to={'/'}
-                            className='self-center text-lg font-semibold whitespace-nowrap sm:text-xl dark:text-white'
+                            to={"/"}
+                            className="self-center text-lg font-semibold whitespace-nowrap sm:text-xl dark:text-white"
                         >
-                            <div className='w-[150px] md:w-[200px]'>
-                                <img src='/web-universe-high-resolution-logo-transparent.png' alt='logo' />
+                            <div className="w-[150px] md:w-[200px]">
+                                <img src="/assets/images/logo_sm.png" alt="logo" />
                             </div>
                         </Link>
                     </div>
-                    <div className='grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-10'>
+                    <div className="grid grid-cols-2 gap-8 sm:mt-4 sm:grid-cols-3 sm:gap-10">
                         <div>
-                            <Footer.Title title='About' />
-                            <Footer.LinkGroup col>
-                                <Link to={'/'}>Ankit's Blog</Link>
-                                <Link to={'/about'}>About</Link>
-                            </Footer.LinkGroup>
-                        </div>
-                        <div>
-                            <Footer.Title title='FoLLOW US' />
+                            <Footer.Title title="Follow Us" />
                             <Footer.LinkGroup col>
                                 <Footer.Link
-                                    href='https://github.com/webdevankit07'
-                                    target='_blank'
-                                    rel='noopener noreferrer'
+                                    href="https://github.com/ivanivanov101"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
                                 >
                                     Github
                                 </Footer.Link>
                                 <Footer.Link
-                                    href='https://www.linkedin.com/in/webdevankit/'
-                                    target='_blank'
-                                    rel='noopener noreferrer'
+                                    href="https://www.linkedin.com/in/ivan-ivanov-101/"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
                                 >
-                                    Linkdln
+                                    LinkedIn
                                 </Footer.Link>
                             </Footer.LinkGroup>
                         </div>
                         <div>
-                            <Footer.Title title='LGEAL' />
+                            <Footer.Title title="LEGAL" />
                             <Footer.LinkGroup col>
-                                <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>
+                                <Footer.Link href="#" target="_blank" rel="noopener noreferrer">
                                     Privacy Policy
                                 </Footer.Link>
-                                <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>
+                                <Footer.Link href="#" target="_blank" rel="noopener noreferrer">
                                     Terms & Conditions
                                 </Footer.Link>
                             </Footer.LinkGroup>
                         </div>
                     </div>
                 </div>
-                <Footer.Divider />
-                <div className='w-full sm:flex sm:items-center sm:justify-between sm:px-5'>
+                <div className="w-full sm:flex sm:items-center sm:justify-between sm:px-5 my-2">
                     <Footer.Copyright
-                        href='https://github.com/webdevankit07'
-                        by='WebDev Ankit'
-                        year={new Date().getFullYear()}
+                        by="Ivan Ivanov"
+                        year={2024}
                     />
-                    <div className='flex gap-6 mt-4 md:mt-2 md:justify-center'>
-                        <Footer.Icon href='https://www.linkedin.com/in/webdevankit/' icon={BsLinkedin} />
-                        <Footer.Icon href='https://github.com/webdevankit07' icon={BsGithub} />
-                        <Footer.Icon href='#' icon={BsInstagram} />
-                        <Footer.Icon href='#' icon={BsFacebook} />
-                        <Footer.Icon href='#' icon={BsTwitter} />
+                    <div className="flex gap-8 mt-4 md:mt-0 md:justify-center">
+                        <Footer.Icon
+                            href="https://www.linkedin.com/in/ivan-ivanov-101/"
+                            icon={BsLinkedin}
+                        />
+                        <Footer.Icon
+                            href="https://github.com/ivanivanov101/"
+                            icon={BsGithub}
+                        />
                     </div>
                 </div>
             </div>
@@ -77,4 +74,4 @@ const FooterCom = () => {
     );
 };
 
-export default FooterCom;
+export default FooterComponent;
--- Frontend/src/components/Header/Header.tsx
+++ Frontend/src/components/Header/Header.tsx
@@ -1,20 +1,23 @@
-import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Avatar, Button, Dropdown, Navbar, TextInput } from "flowbite-react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 // React icons...
-import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon, FaSun } from 'react-icons/fa';
-import { useAppDispatch, useAppSelector } from '../../store/storeHooks';
-import { toggleTheme } from '../../store/features/theme/themeSlice';
-import { deleteUserFailure, signoutUserSuccess } from '../../store/features/user/userSlice';
-import { useEffect, useState } from 'react';
-import { handleAxiosError } from '../../utils/utils';
-import { Axios } from '../../config/api';
+import { AiOutlineSearch } from "react-icons/ai";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useAppDispatch, useAppSelector } from "../../store/storeHooks";
+import { toggleTheme } from "../../store/features/theme/themeSlice";
+import {
+    deleteUserFailure,
+    signoutUserSuccess,
+} from "../../store/features/user/userSlice";
+import { FormEvent, useEffect, useState } from "react";
+import { handleAxiosError } from "../../utils/utils";
+import { Axios } from "../../config/api";
 
 const Header = () => {
     const { theme } = useAppSelector((state) => state.theme);
     const { currentUser } = useAppSelector((state) => state.user);
-    const [searchTerm, setSearchTerm] = useState<string>('');
+    const [searchTerm, setSearchTerm] = useState<string>("");
     const location = useLocation();
     const path = useLocation().pathname;
     const dispatch = useAppDispatch();
@@ -22,7 +25,7 @@ const Header = () => {
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
-        const searchTermFromUrl = urlParams.get('searchTerm');
+        const searchTermFromUrl = urlParams.get("searchTerm");
         if (searchTermFromUrl) {
             setSearchTerm(searchTermFromUrl);
         }
@@ -33,71 +36,81 @@ const Header = () => {
         try {
             await Axios.post(`/user/logout/${currentUser?._id}`);
             dispatch(signoutUserSuccess());
-            navigate('/');
+            navigate("/");
         } catch (error) {
             const err = await handleAxiosError(error);
             dispatch(deleteUserFailure(err));
         }
     };
 
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const urlParams = new URLSearchParams(location.search);
-        urlParams.set('searchTerm', searchTerm);
+        urlParams.set("searchTerm", searchTerm);
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
     };
 
     return (
-        <Navbar className='border-b-2'>
+        <Navbar className="border-b-4 [box-shadow:0_1px_18px_-5px_rgba(0,0,0,0.41)] dark:bg-[rgb(20,19,31)] dark:bg-[linear-gradient(90deg,_rgba(20,19,31,0.9669117647058824)_0%,_rgba(41,47,55,1)_37%,_rgba(10,33,37,1)_100%)]">
             <Link
-                to={'/'}
-                className='relative self-center text-sm font-semibold whitespace-nowrap sm:text-xl dark:text-white'
+                to={"/"}
+                className="relative self-center inline-block text-sm font-semibold whitespace-nowrap sm:text-xl dark:text-white"
             >
-                <div className='absolute -top-3 w-[150px]'>
-                    <img src='/web-universe-high-resolution-logo-transparent.png' alt='logo' />
+                <div className="static inline-block w-[50px]">
+                    <img src="/assets/images/logo.png" alt="logo" />
                 </div>
+                <div className="relative inline-block  left-3 ">Filmio</div>
             </Link>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit} className="w-auto">
                 <TextInput
-                    type='text'
-                    placeholder='Search...'
+                    type="text"
+                    placeholder="Search..."
                     rightIcon={AiOutlineSearch}
-                    className='hidden lg:inline'
+                    className="hidden sm:inline w-96 focus:border-none"
                     value={searchTerm}
                     onChange={(e) => setSearchTerm(e.target.value)}
                 />
             </form>
-            <div className='flex gap-2 md:order-2'>
-                <Button className='w-12 h-10' color='gray' pill onClick={() => dispatch(toggleTheme())}>
-                    {theme === 'dark' ? <FaMoon /> : <FaSun />}
+            <div className="flex gap-2 md:order-2">
+                <Button
+                    className="w-12 h-10 rounded-md"
+                    color="gray"
+                    pill
+                    onClick={() => dispatch(toggleTheme())}
+                >
+                    {theme === "dark" ? <FaMoon /> : <FaSun />}
                 </Button>
                 {currentUser ? (
                     <Dropdown
                         inline
                         arrowIcon={false}
-                        label={<Avatar alt='user' img={currentUser.profilePicture} rounded />}
+                        label={
+                            <Avatar alt="user" img={currentUser.profilePicture} rounded />
+                        }
                     >
                         <Dropdown.Header>
-                            <span className='block text-sm'>@{currentUser.userName}</span>
-                            <span className='block text-sm font-medium'>{currentUser.email}</span>
+                            <span className="block text-sm">@{currentUser.userName}</span>
+                            <span className="block text-sm font-medium">
+                {currentUser.email}
+              </span>
                         </Dropdown.Header>
                         {currentUser.isAdmin && (
-                            <Link to={'/dashboard'}>
+                            <Link to={"/dashboard"}>
                                 <Dropdown.Item>Dashboard</Dropdown.Item>
                             </Link>
                         )}
-                        <Link to={'/dashboard?tab=profile'}>
+                        <Link to={"/dashboard?tab=profile"}>
                             <Dropdown.Item>Profile</Dropdown.Item>
                         </Link>
 
                         <Dropdown.Item onClick={handleSignout}>
-                            <span className='w-full text-center'>Sign out</span>
+                            <span className="w-full text-center">Sign out</span>
                         </Dropdown.Item>
                     </Dropdown>
                 ) : (
-                    <Link to={'/sign-in'}>
-                        <Button gradientDuoTone='purpleToBlue' outline>
+                    <Link to={"/sign-in"}>
+                        <Button className="text-white bg-[#63d052] hover:bg-[#81d973] focus:outline-none focus:ring-[#81d973] font-medium rounded-md text-sm py-0.5 text-center dark:bg-[#63d052] dark:hover:bg-[#63d052] dark:focus:ring-[#81d973]">
                             Sign In
                         </Button>
                     </Link>
