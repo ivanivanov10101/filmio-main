00040fdbed818d96e398e8c7c527e89b0acd9921
Ivan Ivanov <78234071+ivanivanov10101@users.noreply.github.com>
2024-04-19 17:41:57 +0300

    updates to pages

--- Frontend/src/pages/Search.tsx
+++ Frontend/src/pages/Search.tsx
@@ -1,10 +1,10 @@
-import { Button, Select, Spinner, TextInput } from 'flowbite-react';
-import { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
-import PostCard from '../components/PostCard';
-import { Post } from './PostPage';
-import { handleAxiosError } from '../utils/utils';
-import { Axios } from '../config/api';
+import { Button, Select, Spinner, TextInput } from "flowbite-react";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import PostCard from "../components/PostCard";
+import { Post } from "./PostPage";
+import { handleAxiosError } from "../utils/utils";
+import { Axios } from "../config/api";
 
 type SidebarDataType = {
     searchTerm: string;
@@ -14,9 +14,9 @@ type SidebarDataType = {
 
 const Search = () => {
     const [sidebarData, setSidebarData] = useState<SidebarDataType>({
-        searchTerm: '',
-        sort: 'desc',
-        category: 'all',
+        searchTerm: "",
+        sort: "desc",
+        category: "all",
     });
     const [posts, setPosts] = useState<Post[] | []>([]);
     const [loading, setLoading] = useState<boolean>(false);
@@ -28,9 +28,9 @@ const Search = () => {
 
     useEffect(() => {
         const urlParams = new URLSearchParams(location.search);
-        const searchTermFromUrl = urlParams.get('searchTerm');
-        const sortFromUrl = urlParams.get('sort');
-        const categoryFromUrl = urlParams.get('category');
+        const searchTermFromUrl = urlParams.get("searchTerm");
+        const sortFromUrl = urlParams.get("sort");
+        const categoryFromUrl = urlParams.get("category");
         if (searchTermFromUrl || sortFromUrl || categoryFromUrl) {
             setSidebarData({
                 ...sidebarData,
@@ -61,16 +61,18 @@ const Search = () => {
         })();
     }, [location.search]);
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
-        if (e.target.id === 'searchTerm') {
+    const handleChange = (
+        e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+    ) => {
+        if (e.target.id === "searchTerm") {
             setSidebarData({ ...sidebarData, searchTerm: e.target.value });
         }
-        if (e.target.id === 'sort') {
-            const order = e.target.value || 'desc';
+        if (e.target.id === "sort") {
+            const order = e.target.value || "desc";
             setSidebarData({ ...sidebarData, sort: order });
         }
-        if (e.target.id === 'category') {
-            const category = e.target.value || 'all';
+        if (e.target.id === "category") {
+            const category = e.target.value || "all";
             setSidebarData({ ...sidebarData, category });
         }
     };
@@ -78,9 +80,10 @@ const Search = () => {
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const urlParams = new URLSearchParams(location.search);
-        sidebarData.searchTerm && urlParams.set('searchTerm', sidebarData.searchTerm);
-        sidebarData.sort && urlParams.set('sort', sidebarData.sort);
-        sidebarData.category && urlParams.set('category', sidebarData.category);
+        sidebarData.searchTerm &&
+        urlParams.set("searchTerm", sidebarData.searchTerm);
+        sidebarData.sort && urlParams.set("sort", sidebarData.sort);
+        sidebarData.category && urlParams.set("category", sidebarData.category);
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
     };
@@ -104,62 +107,77 @@ const Search = () => {
     };
 
     return (
-        <div className='flex flex-col md:flex-row'>
-            <div className='border-b border-gray-500 p-7 md:border-r md:min-h-screen'>
-                <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
-                    <div className='flex items-center gap-2'>
-                        <label className='font-semibold whitespace-nowrap'>Search Term:</label>
+        <div className="flex flex-col md:flex-row">
+            <div className="border-b border-gray-500 p-7 md:border-r md:min-h-screen">
+                <form className="flex flex-col gap-8" onSubmit={handleSubmit}>
+                    <div className="flex items-center gap-2">
+                        <label className="font-semibold whitespace-nowrap">
+                            Search Term:
+                        </label>
                         <TextInput
-                            placeholder='Search...'
-                            id='searchTerm'
-                            type='text'
+                            placeholder="Search..."
+                            id="searchTerm"
+                            type="text"
                             value={sidebarData.searchTerm}
                             onChange={handleChange}
                         />
                     </div>
-                    <div className='flex items-center gap-2'>
-                        <label className='font-semibold'>Sort:</label>
-                        <Select onChange={handleChange} value={sidebarData.sort} id='sort'>
-                            <option value='desc'>Latest</option>
-                            <option value='asc'>Oldest</option>
+                    <div className="flex items-center gap-2">
+                        <label className="font-semibold">Sort:</label>
+                        <Select onChange={handleChange} value={sidebarData.sort} id="sort">
+                            <option value="desc">Latest</option>
+                            <option value="asc">Oldest</option>
                         </Select>
                     </div>
-                    <div className='flex items-center gap-2'>
-                        <label className='font-semibold'>Category:</label>
-                        <Select onChange={handleChange} value={sidebarData.category} id='category'>
-                            <option value='all'>All</option>
-                            <option value='uncategorized'>Uncategorized</option>
-                            <option value='webtech'>WebTech</option>
-                            <option value='history'>History</option>
-                            <option value='science'>Science</option>
-                            <option value='science-fiction'>Science & Fiction</option>
-                            <option value='mystery'>Mystery</option>
-                            <option value='facts'>Facts</option>
+                    <div className="flex items-center gap-2">
+                        <label className="font-semibold">Category:</label>
+                        <Select
+                            onChange={handleChange}
+                            value={sidebarData.category}
+                            id="category"
+                        >
+                            <option value="all">All</option>
+                            <option value="uncategorized">Uncategorized</option>
+                            <option value="movies">Movies</option>
+                            <option value="tvseries">TV Series</option>
+                            {/*<option value="science">Science</option>*/}
+                            {/*<option value="science-fiction">Science & Fiction</option>*/}
+                            {/*<option value="mystery">Mystery</option>*/}
+                            {/*<option value="facts">Facts</option>*/}
                         </Select>
                     </div>
-                    <Button type='submit' outline gradientDuoTone='purpleToPink'>
+                    <Button type="submit" className="text-white bg-[#63d052] hover:bg-[#81d973] focus:outline-none focus:ring-[#81d973] font-medium rounded-md text-sm py-0.5 text-center dark:bg-[#63d052] dark:hover:bg-[#63d052] dark:focus:ring-[#81d973]">
                         Apply Filters
                     </Button>
                 </form>
             </div>
-            <div className='w-full'>
-                <h1 className='p-3 mt-5 text-3xl font-semibold border-gray-500 sm:border-b '>Posts results:</h1>
-                <div className='flex flex-wrap gap-4 p-7'>
-                    {!loading && posts?.length === 0 && <p className='text-xl text-gray-500'>No posts found.</p>}
+            <div className="w-full">
+                <h1 className="p-3 mt-5 text-3xl font-semibold border-gray-500 sm:border-b ">
+                    Posts results:
+                </h1>
+                <div className="flex flex-wrap gap-4 p-7">
+                    {!loading && posts?.length === 0 && (
+                        <p className="text-xl text-gray-500">No posts found.</p>
+                    )}
                     {loading && (
-                        <div className='grid w-full min-h-screen place-content-center'>
-                            <Spinner size={'xl'} />
+                        <div className="grid w-full min-h-screen place-content-center">
+                            <Spinner size={"xl"} />
                         </div>
                     )}
-                    {!loading && posts && posts.map((post) => <PostCard key={post._id} post={post} />)}
+                    {!loading &&
+                        posts &&
+                        posts.map((post) => <PostCard key={post._id} post={post} />)}
                     {showMore && !showMoreLoading && (
-                        <button className='self-center w-full text-sm text-teal-500 py-7' onClick={handleShowMore}>
+                        <button
+                            className="self-center w-full text-sm text-teal-500 py-7"
+                            onClick={handleShowMore}
+                        >
                             Show more
                         </button>
                     )}
                     {showMoreLoading && (
-                        <div className='grid w-full min-h-20 place-content-center'>
-                            <Spinner size={'xl'} />
+                        <div className="grid w-full min-h-20 place-content-center">
+                            <Spinner size={"xl"} />
                         </div>
                     )}
                 </div>
--- Frontend/src/pages/Signup.tsx
+++ Frontend/src/pages/Signup.tsx
@@ -1,14 +1,15 @@
-import { Button, Label, Spinner, TextInput } from 'flowbite-react';
-import { useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { Link, useNavigate } from 'react-router-dom';
+import { Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+
 
 // React Icons...
-import { BsFillEyeFill, BsFillEyeSlashFill } from 'react-icons/bs';
-import OAuth from '../components/OAuth';
-import { Axios } from '../config/api';
-import { handleAxiosError } from '../utils/utils';
-import ShowAlert from '../components/showAlert';
+import { BsFillEyeFill, BsFillEyeSlashFill } from "react-icons/bs";
+import OAuth from "../components/OAuth";
+import { Axios } from "../config/api";
+import { handleAxiosError } from "../utils/utils";
+import ShowAlert from "../components/showAlert";
 
 type SignUpFormData = {
     fullName: string;
@@ -37,7 +38,7 @@ const Signup = () => {
             setError(null);
             await Axios.post(`/auth/register`, formData);
             setIsLoading(false);
-            navigate('/sign-in');
+            navigate("/sign-in");
         } catch (error) {
             const err = await handleAxiosError(error);
             setError(err);
@@ -46,158 +47,180 @@ const Signup = () => {
     };
 
     return (
-        <div className='my-10'>
-            <div className='flex flex-row items-center justify-center max-w-5xl gap-5 p-3 mx-auto rounded-md'>
+        <div className="my-10">
+            <div className="flex flex-row items-center justify-center max-w-5xl gap-5 p-3 mx-auto rounded-md">
                 {/* Left */}
-                <div className='flex-1 hidden md:block'>
-                    <div className='relative flex flex-col  max-h-[600px] md:max-w-[380px] lg:max-w-[400px] items-center overflow-hidden rounded-md cursor-pointer group border'>
+                <div className="flex-1 hidden md:block">
+                    <div className="relative flex flex-col  max-h-[600px] md:max-w-[380px] lg:max-w-[400px] items-center overflow-hidden rounded-md  group ">
                         <img
-                            src={'/image.jpg'}
-                            className='group-hover:scale-[1.05] transition-all duration-300 ease-in-out h-[700px] w-full'
+                            src="/assets/images/image.jpg"
+                            className="h-[700px] w-full"
                         />
-                        <div className='absolute hidden w-full h-full transition-all duration-300 ease-in-out bg-black opacity-50 group-hover:block'></div>
+                        <div className="absolute hidden w-full h-full"></div>
                         <Link
-                            to={'/'}
-                            className='absolute flex justify-center mb-10 text-4xl font-bold transition-all duration-300 ease-in-out group-hover:bottom-0 -bottom-28 sm:text-xl dark:text-white'
+                            to={"/"}
+                            className="absolute flex justify-center mb-10 text-4xl font-bold -bottom-28 sm:text-xl dark:text-white"
                         >
-                            <img
-                                src='/web-universe-high-resolution-logo-transparent.png'
-                                className='w-[200px]'
-                                alt=''
-                            />
                         </Link>
                     </div>
                 </div>
 
                 {/* Right */}
-                <div className='flex-1'>
-                    <div className='block md:hidden'>
+                <div className="flex-1">
+                    <div className="block md:hidden">
                         <Link
-                            to={'/'}
-                            className='flex justify-center mb-10 text-4xl font-bold sm:text-xl dark:text-white'
+                            to={"/"}
+                            className="flex justify-center mb-10 text-4xl font-bold sm:text-xl dark:text-white"
                         >
-                            <img
-                                src='../../public/web-universe-high-resolution-logo-transparent.png'
-                                className='w-[200px]'
-                                alt=''
-                            />
                         </Link>
                     </div>
-                    <form className='flex flex-col gap-4' onSubmit={handleSubmit(handleFormSubmit)}>
+                    <form
+                        className="flex flex-col gap-4"
+                        onSubmit={handleSubmit(handleFormSubmit)}
+                    >
                         <div>
-                            <Label value='Your fullName' htmlFor='fullname' />
+                            <Label value="Your name" htmlFor="fullname" />
                             <TextInput
-                                type='text'
-                                placeholder='Your name'
-                                id='fullname'
-                                autoComplete='off'
-                                {...register('fullName', {
-                                    required: 'fullname is required',
+                                type="text"
+                                placeholder="Your Name"
+                                id="fullname"
+                                autoComplete="off"
+                                {...register("fullName", {
+                                    required: "Name is required",
                                 })}
                             />
-                            <p className='h-2 px-3 pt-1 pb-3 text-sm text-red-500'>{errors.fullName?.message}</p>
+                            <p className="h-2 px-3 pt-1 pb-3 text-sm text-red-500">
+                                {errors.fullName?.message}
+                            </p>
                         </div>
                         <div>
-                            <Label value='Your userName' htmlFor='username' />
+                            <Label value="Your username" htmlFor="username" />
                             <TextInput
-                                type='text'
-                                placeholder='Username'
-                                id='username'
-                                autoComplete='off'
-                                {...register('userName', {
-                                    required: 'username is required',
+                                type="text"
+                                placeholder="Username"
+                                id="username"
+                                autoComplete="off"
+                                {...register("userName", {
+                                    required: "Username is required",
                                 })}
                             />
-                            <p className='h-2 px-3 pt-1 pb-3 text-sm text-red-500'>{errors.userName?.message}</p>
+                            <p className="h-2 px-3 pt-1 pb-3 text-sm text-red-500">
+                                {errors.userName?.message}
+                            </p>
                         </div>
                         <div>
-                            <Label value='Your email' htmlFor='email' />
+                            <Label value="Your email" htmlFor="email" />
                             <TextInput
-                                type='email'
-                                placeholder='name@company.com'
-                                id='email'
-                                autoComplete='off'
-                                {...register('email', {
-                                    required: 'email is required',
+                                type="email"
+                                placeholder="name@company.com"
+                                id="email"
+                                autoComplete="off"
+                                {...register("email", {
+                                    required: "Email is required",
                                     pattern: {
-                                        value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
-                                        message: 'Invalid Email Id',
+                                        value:
+                                            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/,
+                                        message: "Invalid Email Id",
                                     },
                                     validate: {
                                         blockDomain: (value) => {
-                                            return !value.endsWith('test.com') || 'This is not a valid domain';
+                                            return (
+                                                !value.endsWith("test.com") ||
+                                                "This is not a valid domain"
+                                            );
                                         },
                                         lengthError: (value) => {
-                                            return value.length > 6 || 'please enter a valid email address';
+                                            return (
+                                                value.length > 6 || "please enter a valid email address"
+                                            );
                                         },
                                     },
                                 })}
                             />
-                            <p className='h-2 px-3 pt-1 pb-3 text-sm text-red-500'>{errors.email?.message}</p>
+                            <p className="h-2 px-3 pt-1 pb-3 text-sm text-red-500">
+                                {errors.email?.message}
+                            </p>
                         </div>
-                        <div className='relative'>
-                            <Label value='Your password' htmlFor='password' />
+                        <div className="relative">
+                            <Label value="Your password" htmlFor="password" />
                             <TextInput
-                                type={isPasswordVisible ? 'text' : 'password'}
-                                placeholder='Password'
-                                id='password'
-                                autoComplete='off'
-                                {...register('password', {
-                                    required: 'password is required',
+                                type={isPasswordVisible ? "text" : "password"}
+                                placeholder="Password"
+                                id="password"
+                                autoComplete="off"
+                                {...register("password", {
+                                    required: "Password is required",
                                     validate: {
                                         lengthError: (value) => {
-                                            return value.length >= 8 || 'password must be atleast 8 characters';
+                                            return (
+                                                value.length >= 8 ||
+                                                "password must be atleast 8 characters"
+                                            );
                                         },
                                     },
                                 })}
                             />
                             <span
-                                className='absolute p-1.5 transition-all duration-200 ease-in-out rounded-full cursor-pointer right-2 top-8 hover:bg-blue-900 hover:text-white'
+                                className="absolute p-1.5 transition-all duration-200 ease-in-out rounded-full cursor-pointer right-2 top-8 hover:bg-blue-900 hover:text-white"
                                 onClick={() => setIsPasswordVisible((prev) => !prev)}
                             >
-                                {isPasswordVisible ? <BsFillEyeFill /> : <BsFillEyeSlashFill />}
-                            </span>
-                            <p className='h-2 px-3 pt-1 pb-3 text-sm text-red-500'>{errors.password?.message}</p>
+                {isPasswordVisible ? <BsFillEyeFill /> : <BsFillEyeSlashFill />}
+              </span>
+                            <p className="h-2 px-3 pt-1 pb-3 text-sm text-red-500">
+                                {errors.password?.message}
+                            </p>
                         </div>
                         <div>
-                            <Label value='Confirm your password' htmlFor='confirmPassword' />
+                            <Label value="Confirm your password" htmlFor="confirmPassword" />
                             <TextInput
-                                type='password'
-                                placeholder='confirmPassword'
-                                id='confirmPassword'
-                                autoComplete='off'
-                                {...register('confirmPassword', {
-                                    required: { value: true, message: 'Please confirm the password' },
+                                type="password"
+                                placeholder="confirmPassword"
+                                id="confirmPassword"
+                                autoComplete="off"
+                                {...register("confirmPassword", {
+                                    required: {
+                                        value: true,
+                                        message: "Please confirm the password",
+                                    },
                                     validate: (value) => {
-                                        return watch('password') === value || 'your password does not match';
+                                        return (
+                                            watch("password") === value ||
+                                            "your password does not match"
+                                        );
                                     },
                                 })}
                             />
-                            <p className='h-2 px-3 pt-1 pb-3 text-sm text-red-500'>{errors.confirmPassword?.message}</p>
+                            <p className="h-2 px-3 pt-1 pb-3 text-sm text-red-500">
+                                {errors.confirmPassword?.message}
+                            </p>
                         </div>
-                        <Button gradientDuoTone={'purpleToPink'} type='submit' disabled={isLoading}>
+                        <Button
+                            className="text-white bg-[#63d052] hover:bg-[#81d973] focus:outline-none focus:ring-[#81d973] font-medium rounded-md text-sm py-0.5 text-center dark:bg-[#63d052] dark:hover:bg-[#63d052] dark:focus:ring-[#81d973]"
+                            type="submit"
+                            disabled={isLoading}
+                        >
                             {isLoading ? (
                                 <>
-                                    <Spinner size={'sm'} />
-                                    <span className='pl-3'>Loading...</span>
+                                    <Spinner size={"sm"} />
+                                    <span className="pl-3">Loading...</span>
                                 </>
                             ) : (
-                                'Sign Up'
+                                "Sign Up"
                             )}
                         </Button>
                         <OAuth />
                     </form>
-                    <div className='flex gap-1 mt-5 text-sm'>
+                    <div className="flex gap-1 mt-5 text-sm">
                         <span>Have an account?</span>
-                        <Link to={'/sign-in'} className='text-blue-600 underline'>
+                        <Link to={"/sign-in"} className="text-blue-400 underline hover:text-blue-200">
                             Sign In
                         </Link>
                     </div>
                     {error && (
                         <ShowAlert
                             message={error}
-                            type={'failure'}
-                            className={'mt-5 text-center'}
+                            type={"failure"}
+                            className={"mt-5 text-center"}
                             onClose={() => setError(null)}
                         />
                     )}
