6ac7da12aa4b7e91ded646deeb281f5c07885b13
Ivan Ivanov <78234071+ivanivanov10101@users.noreply.github.com>
2024-04-20 13:42:35 +0300

    search sortng fix asc desc

--- Frontend/src/pages/Search.tsx
+++ Frontend/src/pages/Search.tsx
@@ -125,8 +125,8 @@ const Search = () => {
                     <div className="flex items-center gap-2">
                         <label className="font-semibold">Sort:</label>
                         <Select onChange={handleChange} value={sidebarData.sort} id="sort">
-                            <option value="desc">Latest</option>
-                            <option value="asc">Oldest</option>
+                            <option value="asc">Latest</option>
+                            <option value="desc">Oldest</option>
                         </Select>
                     </div>
                     <div className="flex items-center gap-2">
