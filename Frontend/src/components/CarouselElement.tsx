63340246a6ea71e98fbf79f42b06c3be96cdc277
Ivan Ivanov <78234071+ivanivanov10101@users.noreply.github.com>
2024-05-16 14:48:08 +0300

    lighthouse optimizations

--- Frontend/src/components/CarouselElement.tsx
+++ Frontend/src/components/CarouselElement.tsx
@@ -14,9 +14,7 @@ const CarouselElement = ({ article }: PropsType) => {
           src={article.image}
           alt="post cover"
           className="h-full w-full"
-          loading="lazy"
         />
-        <div className="swiper-lazy-preloader"></div>
       </Link>
       <div className="absolute m-auto p-16 w-full bottom-0 overflow-ellipsis drop-shadow-xl text-white bg-black/[.2] hover:bg-black/[.3]">
         <Link to={`/post/${article.slug}`}>
